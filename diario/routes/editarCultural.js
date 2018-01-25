26f1b7c278ee1df2eedba1ecc36c84a7a96e99bd
Luís Marques <luismarques24@hotmail.com>
2018-01-25 02:21:57 +0000

    erro ao guardar maneira facil data

--- diario/routes/editarCultural.js
+++ diario/routes/editarCultural.js
@@ -7,6 +7,7 @@ var fs = require('fs')
 /* GET home page. */
 
 router.post('/',function(req,res,next) {
+    var images_dir = './public/images/upload/'
     var form= new formidable.IncomingForm();
     var status=""
 
@@ -33,10 +34,10 @@ router.post('/',function(req,res,next) {
         var listaopcoes = opcoes.map(x => fields[x])
         console.log(listaopcoes)
 
-        for(var x in listaopcoes){
-            console.log(listaopcoes[x])
-            if(listaopcoes[x] != "") {
-                Cultural.update({'_id': fields._id}, {$push: {fotografias: listaopcoes[x]}}).exec(function(err,docs){
+        for(var ft in listaopcoes){
+            console.log(listaopcoes[ft])
+            if(listaopcoes[ft] != "") {
+                Cultural.update({'_id': fields._id}, {$push: {fotografias: listaopcoes[ft]}}).exec(function(err,docs){
                     if(!err){
                         console.log("Evento Cultural alterado com sucesso")
                     }
@@ -44,6 +45,40 @@ router.post('/',function(req,res,next) {
                         console.log("Ocurreu um erro tentar editar evento cultural")
                     }
                 })
+            }
+        }
+
+        var i = 0
+        for(x in files){
+            i++
+            if(x.startsWith("foto")){
+                console.log("nome da foto: " + files[x].name)
+                console.log("caminho da foto: " + files[x].path)
+                if(files[x].name != ""){
+                    var extension = files[x].name.split(".")
+                    extension = extension[extension.length-1]
+                    console.log("valor = " + i)
+                    var novoNome = fields._id + "-" + i + "." + extension
+                    var fenviado=files[x].path
+                    var fnovo=images_dir+novoNome
+                    fs.rename(files[x].path, fnovo, function(err3){
+                        if(!err3){
+                            console.log("Ficheiro recebido e guardado com sucesso")
+                        }
+                        else{
+                            console.log("Ocorreram erros na gravação do ficheiro enviado")
+                        }
+                    })
+                    Cultural.update({'_id': fields._id}, {$push: {fotografias: novoNome}}).exec(function(err,docs){
+                        if(!err){
+                            console.log("Evento Cultural alterado com sucesso")
+                        }
+                        else{
+                            console.log("Ocurreu um erro tentar editar evento cultural")
+                        }
+                    })
+
+                }
             }
         }
 

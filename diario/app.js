616e8fb0cde1449172b1e7afaa4443b32e288bde
Samuel Ferreira <Samuel.Ferreira@checkmarx.com>
2018-01-29 02:18:49 +0000

    adicionada a route para importar

--- diario/app.js
+++ diario/app.js
@@ -102,6 +102,7 @@ app.use('/registarAlbum', album)
 app.use('/eliminar', eliminar)
 app.use('/editar',editar)
 app.use('/feedPublico',feedPublico)
+app.use('/importar',importar)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
